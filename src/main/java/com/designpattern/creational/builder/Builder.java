2304dd37aabc080522da21c6fdcc104fc641c898
Zahid Khan <zahid.khan@valebridgetech.com>
2021-11-09 10:40:12 +0530

    Builder pattern

---
+++ src/main/java/com/designpattern/creational/builder/Builder.java
@@ -0,0 +1,64 @@
+package com.designpattern.creational.builder;
+
+import lombok.NoArgsConstructor;
+
+public class Builder {
+
+        public static void main(String[] args) {
+                final Car maruti = Car.builder().model(12).name("Maruti").build();
+                final Car eon = Car.builder().model(13).name("Hyundia Eon").build();
+                System.out.println(maruti);
+                System.out.println(eon);
+        }
+}
+
+@NoArgsConstructor
+class Car {
+        int model;
+        String name;
+
+
+        Car(Builder builder) {
+                model = builder.getModel();
+                name = builder.getName();
+        }
+
+        static Builder builder() {
+                return new Builder();
+        }
+
+        @Override
+        public String toString() {
+                return " " + name + " " + model;
+        }
+
+        @NoArgsConstructor
+        static class Builder {
+                private int model;
+                private String name;
+
+                public int getModel() {
+                        return model;
+                }
+
+                public String getName() {
+                        return name;
+                }
+
+                public Builder model(int model) {
+                        this.model = model;
+                        return this;
+                }
+
+                public Builder name(String name) {
+                        this.name = name;
+                        return this;
+                }
+
+                public Car build() {
+                        Car car = new Car(this);
+                        return car;
+                }
+        }
+}
+
