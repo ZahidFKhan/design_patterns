c1cd0a0f6875ebe80a097ba79959c6a9beb032f4
Zahid_Khan <xahiidkhan@gmail.com>
2025-06-19 09:31:59 +0530

    Add Prototype design pattern

--- src/main/java/com/designpattern/creational/prototype/Student.java
+++ src/main/java/com/designpattern/creational/prototype/Student.java
@@ -1,6 +1,12 @@
 package com.designpattern.creational.prototype;
 
 record Student(int id, long salary, String name) implements Cloneable {
+  // 1st Way
+  Student(Student secondStudent) {
+    this(secondStudent.id(), secondStudent.salary(), secondStudent.name());
+  }
+
+  // 2nd WAY not recommended
   @Override
   protected Object clone() throws CloneNotSupportedException {
     return new Student(id(), salary(), name());
--- src/test/java/com/designpattern/creational/prototype/StudentTest.java
+++ src/test/java/com/designpattern/creational/prototype/StudentTest.java
@@ -9,6 +9,13 @@ import org.junit.jupiter.api.Test;
 class StudentTest {
 
   @Test
+  void testStudentsAreEqualUsingCopyConstructor() {
+    Student actualStudent = new Student(12, 12000, "Zahid");
+    Student clonedStudent = new Student(actualStudent);
+    Assertions.assertThat(actualStudent).isEqualTo(clonedStudent);
+  }
+
+  @Test
   @SneakyThrows
   void testStudentsAreEqual() {
     Student actualStudent = new Student(12, 12000, "Zahid");
